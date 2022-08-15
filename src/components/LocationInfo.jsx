93ded40022bae5f39757a3f09628004e32e71a05
JL-Wariord-GP <jorge.luis-leones@hotmail.com>
2022-08-14 23:18:53 -0500

    finish

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 
-const LocationInfo = ({location}) => {
-  return (
-    <div >
-        {
-            //!INFO
-        }
-        <div className='box-location'>
+const LocationInfo = ({ location }) => {
+    return (
+        <div >
+            {
+                //!INFO
+            }
+            <div className='box-location'>
                 <h2>Citadel of Ricks</h2>
-            <div>
-                <ul className='box-ul-li'>
-                    <li><span>Type: </span>{location?.type}</li>
-                    <li><span>Dimension: </span>{location?.dimension}</li>
-                    <li><span>Population: </span>{location?.residents.length}</li>
-                </ul>
+                <div>
+                    <ul className='box-ul-li'>
+
+                        <li><span>Name: </span>{location?.name}</li>
+                        <li><span>Type: </span>{location?.type}</li>
+                        <li><span>Dimension: </span>{location?.dimension}</li>
+                        <li><span>Population: </span>{location?.residents.length}</li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default LocationInfo
